a4fdf85a5acbc74e5e9d0503021ee292a5a940ba
Nakamura Yuki <111046707+powaaaaa@users.noreply.github.com>
2024-06-05 01:06:17 +0900

    add: hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  console.log("Hello, World!");
 
   return (
     <>
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>Hello, World!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
